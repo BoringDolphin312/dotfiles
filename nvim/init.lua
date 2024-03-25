894403b88099de4997bea98fe7dadcf4893bc9fc
patrick.makimoto <patrick.makimoto@fpf.br>
2024-03-25 14:59:37 -0400

    update

--- nvim/init.lua
+++ nvim/init.lua
@@ -149,3 +149,24 @@ require('lspconfig')['<YOUR_LSP_SERVER>'].setup {
   capabilities = capabilities
 }
 
+-- Setup debug
+
+local dap = require("dap")
+dap.adapters.gdb = {
+  type = "executable",
+  command = "gdb",
+  args = { "-i", "dap" }
+}
+
+dap.configurations.c = {
+  {
+    name = "Launch",
+    type = "gdb",
+    request = "launch",
+    program = function()
+      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+    end,
+    cwd = "${workspaceFolder}",
+    stopAtBeginningOfMainSubprogram = false,
+  },
+}
